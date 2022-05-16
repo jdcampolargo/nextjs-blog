7418390de71920b280d2bfc6c30caf901c354fbf
jdcampolargo <jdcampolargo@hotmail.com>
2022-05-16 11:29:06 -0500

    updating

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <h1 className="title">
         Coming back soon!{' '}
   <Link href="https://juandavidcampolargo.substack.com/">
-    <a>🫠😉</a>
+    <a> 🫠😉</a>
   </Link>
 </h1>
         <p className="description">
@@ -178,3 +178,23 @@ export default function Home() {
     </div>
   )
 }
+// // import Head from 'next/head';
+// // import Layout, { siteTitle } from '../components/layout';
+// // import utilStyles from '../styles/utils.module.css';
+
+// // export default function Home() {
+// //   return (
+// //     <Layout home>
+// //       <Head>
+// //         <title>{siteTitle}</title>
+// //       </Head>
+// //       <section className={utilStyles.headingMd}>
+// //         <p>[Your Self Introduction]</p>
+// //         <p>
+// //           (This is a sample website - you’ll be building a site like this on{' '}
+// //           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+// //         </p>
+// //       </section>
+// //     </Layout>
+// //   );
+// // }
